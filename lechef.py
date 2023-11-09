b880851a5233085dad6b3b9ae02fb5fdfdb92cf9
jdg33 <150250947+jdg33@users.noreply.github.com>
2023-11-09 12:42:28 +0100

    Update lechef.py
    changed model, added progress bar, added idea option

--- lechef.py
+++ lechef.py
@@ -6,41 +6,57 @@ api_key = st.secrets["OAPI"]
 openai.api_key = api_key
 
 def chat_with_model(messages):
-    response = openai.chat.completions.create(
-        model="gpt-4",
+    response = client.chat.completions.create(
+        model="gpt-4-1106-preview",
         messages=messages
     )
     return response.choices[0].message.content
 
 def generate_random_recipe():
     response = chat_with_model([
-        {"role": "system", "content": "You are a French recipe bot. Include ingredients, cooking instructions, and related notes/tips at the end. Please make sure to include a title and description. Format neatly with headers"},
+        {"role": "system", "content": "You are a recipe bot. Include ingredients, cooking instructions, and related notes/tips at the end. Please make sure to include a title and description. Format neatly with headers"},
         {"role": "user", "content": "Generate a random recipe."}
     ])
     return response
 
-def generate_recipe_with_input(idea, diet, allergies, time_constraint):
+def generate_recipe_with_input(diet, allergies, time_constraint):
     response = chat_with_model([
-        {"role": "system", "content": "You are a French recipe bot. Include ingredients, cooking instructions, and related notes/tips at the end. Please make sure to include a title and description. Format neatly with headers"},
-        {"role": "user", "content": f"My idea for a recipe is {idea}. My diet is {diet}, I have allergies to {allergies}, and I have {time_constraint} to make a meal. Generate a recipe."}
+        {"role": "system", "content": "You are a recipe bot. Include ingredients, cooking instructions, and related notes/tips at the end. Please make sure to include a title and description. Format neatly with headers"},
+        {"role": "user", "content": f"My diet is {diet}, I have allergies to {allergies}, and I have {time_constraint} to make a meal. Generate a recipe."}
     ])
     return response
 
-st.title("Frankie Smokes")
+def generate_recipe_with_idea(idea):
+    response = chat_with_model([
+        {"role": "system", "content": "You are a recipe bot. Include ingredients, cooking instructions, and related notes/tips at the end. Please make sure to include a title and description. Format neatly with headers"},
+        {"role": "user", "content": f"My rough idea for a meal is {idea}. Generate a recipe from that."}
+    ])
+    return response
 
-option = st.radio("Choose Option", ("Generate Random Recipe", "Generate Recipe with Input"))
+st.title("What to cook, what to cook...")
+
+option = st.radio("Choose your option", ("Generate Random Recipe", "Generate Recipe with Input","Generate Recipe from an Idea"))
 
 if option == "Generate Random Recipe":
     if st.button("Generate Recipe"):
-        recipe = generate_random_recipe()
-        st.write(f"Your new recipe:\n{recipe}")
+        with st.spinner('Preparing your recipe...'):
+            recipe = generate_random_recipe()
+            st.write(f"Your new recipe:\n{recipe}")
 
 elif option == "Generate Recipe with Input":
-    idea = st.text_input("Do you have any idea what you might want to eat? If you have no clue, leave this blank.")
     diet = st.text_input("What diet do you have? (e.g. vegetarian, vegan, etc.)")
     allergies = st.text_input("What allergies do you have? (comma-separated list, or none)")
     time_constraint = st.text_input("How long do you have to make a meal? (e.g. 30 minutes)")
 
     if st.button("Generate Recipe"):
-        recipe = generate_recipe_with_input(idea, diet, allergies, time_constraint)
-        st.write(f"Your new recipe:\n{recipe}")
+        with st.spinner('Preparing your recipe...'):
+            recipe = generate_recipe_with_input(diet, allergies, time_constraint)
+            st.write(f"Your new recipe:\n{recipe}")
+
+elif option == "Generate Recipe from an Idea":
+    idea = st.text_input("What are you interested in making?")
+
+    if st.button("Generate Recipe"):
+        with st.spinner('Preparing your recipe...'):
+            recipe = generate_recipe_with_idea(idea)
+            st.write(f"Your new recipe:\n{recipe}")
