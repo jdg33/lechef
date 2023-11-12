132217858c82e763acefca901c5ecc335b0ef327
jdg33 <150250947+jdg33@users.noreply.github.com>
2023-11-12 19:43:06 +0100

    Update lechef.py

--- lechef.py
+++ lechef.py
@@ -56,7 +56,7 @@ if option == "Generate Recipe with Variables":
 
 elif option == "Generate Random Recipe":
     if st.button("Generate Recipe"):
-        with open("RECIPESTEXT.txt", "a") as text_file:
+        with open("/main/lechef/RECIPESTEXT.txt", "a") as text_file:
             with st.spinner('Preparing your recipe...'):
                 recipe = generate_random_recipe()
                 st.write(f"Your new recipe:\n{recipe}")
